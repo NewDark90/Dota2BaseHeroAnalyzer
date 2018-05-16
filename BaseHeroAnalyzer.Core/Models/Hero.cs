cec84c05b0d61b272c16261e5558093e8779ac30
Zachary Dow <mr.zachary.dow@gmail.com>
2018-05-15 19:55:07 -0700

    Adding the base .NetCore 2.0 Website and Core Libraries to a new solution.

---
+++ BaseHeroAnalyzer.Core/Models/Hero.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseHeroAnalyzer.Core.Models
+{
+    public class Hero
+    {
+        public decimal BaseStrength { get; set; }
+        public decimal BaseAgility { get; set; }
+        public decimal BaseIntellegence { get; set; }
+        public decimal BaseArmor { get; set; }
+
+        //etc
+    }
+}
---
+++ BaseHeroAnalyzer.Core/Parsers/NpcHeroParser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseHeroAnalyzer.Core.Parsers
+{
+    public class NpcHeroParser
+    {
+    }
+}
